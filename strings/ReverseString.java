32685b1f10cecc6ee77a54c97509b458486be04b
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-04-29 20:49:07 +0530

    feat:Reverse Strings

---
+++ strings/ReverseString.java
@@ -0,0 +1,15 @@
+package com.java.oop.strings;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    static void main(String[] args) {
+        String str = "java";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--){
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+}
